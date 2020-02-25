173af0b43c076654080e0dbf2ac6eec6bf0f17d5
saint <saint@StomimaMAC.local>
2020-02-25 12:06:15 +0800

    add taiwan

---
+++ driving.py
@@ -0,0 +1,19 @@
+#建立Github專案
+#寫台灣部分
+#上傳
+#寫美國部分
+#upload
+#最後else部分
+#upload
+
+country = input ('請問你是哪國人 （台灣/美國)')
+age = input ('請問你的年齡')
+# 型別轉換 casting
+age = int (age)
+if country == '台灣':
+	if age >= 18 :
+		print ('你可以考駕照')
+	else :
+		print ('毛長齊再來')
+
+     
